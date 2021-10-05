efd16d19f3e2cbf9e2f5e13f91390dea2f4dbaff
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-10-05 08:46:12 -0300

    slider

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ImgSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap></Wrap>
+    </Carousel>
+  );
+};
+
+export default ImgSlider;
+
+const Carousel = styled(Slider)``;
+const Wrap = styled.div``;
