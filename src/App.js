a9d5d6d162f0209666eb9f54f5a5f921d3ce3703
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-10-05 19:34:55 -0300

    login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/detail" exact>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/detail">
             <Detail />
           </Route>
           <Route path="/">
