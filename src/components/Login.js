831ddd182cd83884759d86e9a001807c274d79ce
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-10-06 10:39:35 -0300

    firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,12 @@ const Login = () => {
       <ContentAction>
         <CALogoOne src="/images/cta-logo-one.svg" />
         <SignUp>GET ALL THERE</SignUp>
+        <Description>
+          Get Premier Access to Raya and the Last Dragon for an additional fee
+          with a Disney+ subscription. As of 03/26/21, the price of Disney+ and
+          The Disney Bundle will increase by $1.
+        </Description>
+        <CALogoTwo src="/images/cta-logo-two.png" />
       </ContentAction>
     </Container>
   );
@@ -18,7 +24,7 @@ const Container = styled.div`
   position: relative;
   height: calc(100vh - 70px);
   display: flex;
-  align-items: center;
+  align-items: top;
   justify-content: center;
 
   &:before {
@@ -31,6 +37,7 @@ const Container = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
+    opacity: 0.7;
     z-index: -1;
   }
 `;
@@ -38,9 +45,11 @@ const Container = styled.div`
 const ContentAction = styled.div`
   max-width: 650px;
   padding: 80px 40px;
-  width: 80%;
+  width: 90%;
   display: flex;
   flex-direction: column;
+  margin-top: 100px;
+  align-items: center;
 `;
 const CALogoOne = styled.img``;
 const SignUp = styled.a`
@@ -61,4 +70,15 @@ const SignUp = styled.a`
   &:hover {
     background: #0483ee;
   }
+`;
+
+const Description = styled.p`
+  font-size: 11px;
+  letter-spacing: 1.5px;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const CALogoTwo = styled.img`
+  width: 90%;
 `;
